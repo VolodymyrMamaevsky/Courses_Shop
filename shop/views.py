3310cc44cd79e91fb52f807660255911ebdd1aab
VolodymyrMamaevsky <vova.moder@gmail.com>
2023-12-06 11:26:38 +0200

    add base.html & single_course.html

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Course
 
 
 def index(request):
     courses = Course.objects.all()
     return render(request, "courses.html", {"courses": courses})
+
+
+def single_course(request, course_id):
+    try:
+        course = Course.objects.get(pk=course_id)
+        return render(request, "single_course.html", {"course": course})
+    except Course.DoesNotExist:
+        raise Http404()
