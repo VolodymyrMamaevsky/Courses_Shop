08729fa4eed535ae9dbc80c85842c69cbdebd569
VolodymyrMamaevsky <vova.moder@gmail.com>
2023-12-07 09:45:57 +0200

    change directory for templates & add rest api

--- django_backend/urls.py
+++ django_backend/urls.py
@@ -15,5 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api.models import CourseResource, CategoryResourse
 
-urlpatterns = [path("admin/", admin.site.urls), path("shop/", include("shop.urls"))]
+course_resource = CourseResource()
+category_resource = CategoryResourse()
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("shop/", include("shop.urls")),
+    path("api/", include(course_resource.urls)),
+    path("api/", include(category_resource.urls)),
+]
--- shop/views.py
+++ shop/views.py
@@ -5,12 +5,12 @@ from .models import Course
 
 def index(request):
     courses = Course.objects.all()
-    return render(request, "courses.html", {"courses": courses})
+    return render(request, "shop/courses.html", {"courses": courses})
 
 
 def single_course(request, course_id):
     try:
         course = Course.objects.get(pk=course_id)
-        return render(request, "single_course.html", {"course": course})
+        return render(request, "shop/single_course.html", {"course": course})
     except Course.DoesNotExist:
         raise Http404()
